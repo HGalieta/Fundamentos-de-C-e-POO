d8a2ccab95069773f4a65543a87079328710c85c
Henrique Galieta <100798441+HGalieta@users.noreply.github.com>
2022-05-11 12:50:36 -0300

    Criação de API em aula
    Aula de  Criação de REST API, HTTP e Injeção de Dependência com .NET 6.

---
+++ DevTrackR.API/Controllers/PackagesController.cs
@@ -0,0 +1,65 @@
+﻿using DevTrackR.API.Entities;
+using DevTrackR.API.Models;
+using DevTrackR.API.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevTrackR.API.Controllers
+{
+    [ApiController]
+    [Route("api/packages")]
+    public class PackagesController : ControllerBase
+    {
+        private readonly DevTrackRContext _context;
+        public PackagesController(DevTrackRContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var packages = _context.Packages;
+
+            return Ok(packages);
+        }
+
+        [HttpGet("{code}")]
+        public IActionResult GetByCode(string code)
+        {
+            var package = _context.Packages.SingleOrDefault(p => p.Code == code);
+
+            if(package == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(package);
+        }
+
+        [HttpPost]
+        [ActionName(nameof(Post))]
+        public IActionResult Post(AddPackageInputModel model)
+        {
+            var package = new Package(model.Title, model.Weight);
+
+            _context.Packages.Add(package);
+
+            return CreatedAtAction("GetByCode", new { code = package.Code }, package);
+        }
+
+        [HttpPost("{code}/updates")]
+        public IActionResult PostUpdate(string code, AddPackageUpdateInputModel model)
+        {
+            var package = _context.Packages.SingleOrDefault(p => p.Code == code);
+
+            if (package == null)
+            {
+                return NotFound();
+            }
+
+            package.AddUpdate(model.Status, model.Delivered);
+
+            return NoContent();
+        }
+    }
+}
